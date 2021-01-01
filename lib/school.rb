cf0c5bd262fc9d81d7fc468408b64a3fbd05f8e1
Luis Castillo <raul@luiss-mbp.myfiosgateway.com>
2020-12-31 19:01:44 -0500

    commit tree a9b107f77db11e2f5f564102e9fd11f825021c25

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,49 @@
 # code here!
+
+class School
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+    @roster
+    end
+
+    def add_student(name, grade)
+        if(roster[grade])
+            roster[grade] << name
+        else
+            roster[grade] = []
+            roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+
+        roster.map do |key, value|
+            value.sort!
+        end
+        roster.sort.to_h
+    end
+end
+
+# School
+#   ::new
+#     has an empty roster when initialized
+#   #add_student
+#     is able to add a student
+#     is able to add multiple students to a class (grade)
+#     is able to add students to different grades
+#   #grade
+#     is able to retreive students from a grade
+#   #sort
+#     is able to sort the students
+
+# Finished in 0.0039 seconds (files took 0.11029 seconds to load)
+# 6 examples, 0 failures
